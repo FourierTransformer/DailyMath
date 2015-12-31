cf6a6694c4464ec6af01afef70cc7f6fda13af0e
Shakil Thakur <shakil.thakur@gmail.com>
2015-12-30 20:54:43 -0500

    looks like there was a minor issue in q with version's id sequencing. This was tested from a restore of the main database.

--- database/sc.01.00.0002.sql
+++ database/sc.01.00.0002.sql
@@ -3,3 +3,7 @@ alter table versions add column date_applied2 timestamp DEFAULT now() NOT NULL;
 update versions set date_applied2 = date_applied;
 alter table versions drop column date_applied;
 alter table versions rename date_applied2 to date_applied;
+
+-- this is an issue in the q (and likely prod) database...
+SELECT setval('versions_id_seq', (SELECT MAX(id) FROM versions));
+
